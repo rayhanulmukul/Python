a5f9042c808f7c23c2c38b75b8462013029f4f7c
Md Rayhanul Islam <73143090+rayhanul2008@users.noreply.github.com>
2024-03-21 19:10:31 +0600

    Code Runner

---
+++ pythonProject/DataType.py
@@ -0,0 +1,18 @@
+x = 'sdfsd 324234 sdf'
+#Print(x)
+
+y = 34234.435
+print(type(y))
+
+a = "Hi"
+b = "My Name is RAM"
+print(a + ' ' + b)
+
+Bool = True
+print(bool, type(Bool))
+
+a = 8
+b = 7
+print(a, ">", b, " = ", a > b)
+
+print(f"this is string arithmetic {232 + 232}")
---
+++ pythonProject/main.py
@@ -0,0 +1,3 @@
+# hello world
+rayhan = "Hi My Name is Rayhanul Islam"
+print(rayhan)
